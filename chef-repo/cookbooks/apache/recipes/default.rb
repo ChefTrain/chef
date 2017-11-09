dd18c31ddb2b0b1fd0a757f288e46eb0d9574d13
okram999 <nirish.okram@gmail.com>
2017-11-09 12:09:01 -0600

    updating day4

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -1,22 +1,51 @@
 #
-# Cookbook:: apache
+# Cookbook Name:: apache
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
 
-#install package
+package "httpd" do
+  action :install
+end
 
-node.default["apache"]["indexfile"] = "index2.html"
-
-package 'httpd'
-
-#Start the service and make it persistent
 service "httpd" do
   action [ :enable, :start ]
 end
 
-#Set up a dummy index.html
-cookbook_file "/var/www/html/index.html" do
-  source node["apache"]["indexfile"]
-  mode "0644"
+execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak" do
+  only_if do
+    File.exist?("/etc/httpd/conf.d/welcome.conf")
+  end
+  notifies :restart, "service[httpd]"
+end
+
+node['apache']['sites'].each do |site_name, site_data|
+  document_root = "/srv/apache/#{site_name}"
+
+  template "/etc/httpd/conf.d/#{site_name}.conf" do
+    source "custom.erb"
+    mode "0644"
+    variables(
+      :document_root => document_root,
+      :port => site_data['port']
+    )
+    notifies :restart, "service[httpd]"
+  end
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    mode "0644"
+    variables(
+      :site_name => site_name,
+      :port => site_data['port']
+    )
+  end
 end
